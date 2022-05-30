a7ec8b9c89a5c4811989419e84d949a09e296dc0
Christian Lecherbauer <christian.lecherbauer@googlemail.com>
2022-05-30 11:55:43 +0200

    [VYZN-126] implement github test workflow

---
+++ src/calculator.spec.ts
@@ -0,0 +1,19 @@
+import {
+  AcousticRatingCalculator,
+  ExternalAcousticRating,
+  IFCItem,
+} from "../src/calculator";
+
+describe("test_test", () => {
+  test("just a test for testing tests", () => {
+    const ifcItems: IFCItem[] = [];
+    const externalAcousticRating: ExternalAcousticRating =
+      new ExternalAcousticRating();
+    const calculator = new AcousticRatingCalculator(
+      ifcItems,
+      externalAcousticRating
+    );
+
+    expect(calculator.calculate()).toEqual("tests are working");
+  });
+});
